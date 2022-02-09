f44dd41daeadb71f7337925041990a1726a0171d
jmiko4 <jmiko4@byui.edu>
2022-02-09 14:47:24 -0700

    yee haw

--- game.py
+++ game.py
@@ -1,6 +1,9 @@
 import pygame
 from pygame.locals import *
 
+background_color = (234, 212, 252)
+screen = pygame.display.set_mode((300, 300))
+screen.fill(background_color)
 #test
 # this is a test to see if it works
 
