b8fddc5605879308e75e96cb5218f45d79ed3e64
lehig <97483848+lehig@users.noreply.github.com>
2022-02-16 14:24:47 -0700

    wassup

--- game.py
+++ game.py
@@ -1,14 +1,3 @@
 import pygame
 from pygame.locals import *
-
-background_color = (234, 212, 252)
-screen = pygame.display.set_mode((300, 300))
-screen.fill(background_color)
-#test
-# this is a test to see if it works
-
-# look we're doing more
-
-#good morning friends and family
-
-#game stuff
+print("edit")
