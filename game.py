46bc6a563521a46695a2b42d702bd4fad8eddebe
jmiko4 <jmiko4@byui.edu>
2022-02-22 14:34:33 -0700

    added stuff

--- game.py
+++ game.py
@@ -5,14 +5,26 @@ print("edit")
 pygame.init()
 vec = pygame.math.Vector2  # 2 for two dimensional
  
-HEIGHT = 850
-WIDTH = 800
+HEIGHT = 720
+WIDTH = 1280
 ACC = 0.5
 FRIC = -0.12
 FPS = 60
+
+WHITE = (255,255,255)
+DARKBLUE = (36,90,190)
+LIGHTBLUE = (0,176,240)
+RED = (255,0,0)
+ORANGE = (255,100,0)
+YELLOW = (255,255,0)
+
+score = 0
+lives = 3
+
+carryOn = True
  
 FramePerSec = pygame.time.Clock()
  
 displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Game")
+pygame.display.set_caption("Breakout")
 
