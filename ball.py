13c2e6d2dd7e3bfc1022ce2461bd62a95d405a4f
lehi gracia <lehigraciaiii@gmail.com>
2022-02-28 14:47:16 -0700

    end game

--- ball.py
+++ ball.py
@@ -1,27 +1,33 @@
 import pygame
-from pygame.locals import *
+from random import randint
 
-class ball():
-
-    def __init__(self, velocity):
-        #establish variables
-        self.center_x = 200
-        self.center_y = 200
-        self.velocity_dx = velocity
-        self.velocity_dy = velocity
-
-    def draw(self, screen, color):
-        #draw the ball to the screen
-        pygame.draw.circle(screen, color, [self.center_x, self.center_y], 10)
-
-    def advance(self):
-        self.center_x += self.velocity_dx
-        self.center_y += self.velocity_dy
-
-    #changes the vertical direction of the ball
-    def bounce_vertical(self):
-        self.velocity_dy *= -1
-
-    #changes the horizontal direction of the ball
-    def bounce_horizontal(self):
-        self.velocity_dx *= -1
+BLACK = (0, 0, 0)
+ 
+class Ball(pygame.sprite.Sprite):
+    #This class represents a ball. It derives from the "Sprite" class in Pygame.
+    
+    def __init__(self, color, width, height):
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+        
+        # Pass in the color of the ball, its width and height.
+        # Set the background color and set it to be transparent
+        self.image = pygame.Surface([width, height])
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+ 
+        # Draw the ball (a rectangle!)
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+        
+        self.velocity = [randint(4,8),randint(-8,8)]
+        
+        # Fetch the rectangle object that has the dimensions of the image.
+        self.rect = self.image.get_rect()
+        
+    def update(self):
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+          
+    def bounce(self):
+        self.velocity[0] = -self.velocity[0]
+        self.velocity[1] = randint(-8,8)
--- game.py
+++ game.py
@@ -1,17 +1,13 @@
+#Import the pygame library and initialise the game engine
 import pygame
-from pygame.locals import *
-from ball import ball
+#Let's import the Paddle Class & the Ball Class
 from paddle import Paddle
+from ball import Ball
+from brick import Brick
 
 pygame.init()
-vec = pygame.math.Vector2  # 2 for two dimensional
- 
-HEIGHT = 720
-WIDTH = 800
-ACC = 0.5
-FRIC = -0.12
-FPS = 60
 
+# Define some colors
 WHITE = (255,255,255)
 DARKBLUE = (36,90,190)
 LIGHTBLUE = (0,176,240)
@@ -19,71 +15,123 @@ RED = (255,0,0)
 ORANGE = (255,100,0)
 YELLOW = (255,255,0)
 
-# --- Drawing the paddle sprite
-paddle = Paddle(WHITE, 100, 10)
-paddle.rect.x = 700
-paddle.rect.y = 700
- 
-#This will be a list that will contain all the sprites we intend to use in our game.
-all_sprites_list = pygame.sprite.Group()
- 
-# Add the paddle to the list of sprites
-all_sprites_list.add(paddle)
-
 score = 0
 lives = 3
-ball_velocity = 5
 
+# Open a new window
+size = (800, 600)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Breakout Game")
+
+#This will be a list that will contain all the sprites we intend to use in our game.
+all_sprites_list = pygame.sprite.Group()
+
+#Create the Paddle
+paddle = Paddle(LIGHTBLUE, 100, 10)
+paddle.rect.x = 350
+paddle.rect.y = 560
+
+#Create the ball sprite
+ball = Ball(WHITE,10,10)
+ball.rect.x = 345
+ball.rect.y = 195
+
+all_bricks = pygame.sprite.Group()
+for i in range(7):
+    brick = Brick(RED,80,30)
+    brick.rect.x = 60 + i* 100
+    brick.rect.y = 60
+    all_sprites_list.add(brick)
+    all_bricks.add(brick)
+for i in range(7):
+    brick = Brick(ORANGE,80,30)
+    brick.rect.x = 60 + i* 100
+    brick.rect.y = 100
+    all_sprites_list.add(brick)
+    all_bricks.add(brick)
+for i in range(7):
+    brick = Brick(YELLOW,80,30)
+    brick.rect.x = 60 + i* 100
+    brick.rect.y = 140
+    all_sprites_list.add(brick)
+    all_bricks.add(brick)
+
+# Add the paddle and the ball to the list of sprites
+all_sprites_list.add(paddle)
+all_sprites_list.add(ball)
+
+# The loop will carry on until the user exits the game (e.g. clicks the close button).
 carryOn = True
- 
-FramePerSec = pygame.time.Clock()
- 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Breakout")
 
+# The clock will be used to control how fast the screen updates
 clock = pygame.time.Clock()
 
-ball = ball(ball_velocity)
-
+# -------- Main Program Loop -----------
 while carryOn:
     # --- Main event loop
     for event in pygame.event.get(): # User did something
         if event.type == pygame.QUIT: # If user clicked close
               carryOn = False # Flag that we are done so we exit this loop
-        elif event.type==pygame.KEYDOWN:
-                if event.key==pygame.K_x: #Pressing the x Key will quit the game
-                     carryOn=False
 
     #Moving the paddle when the use uses the arrow keys
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        paddle.moveLeft(5, 0)
+        paddle.moveLeft(5)
     if keys[pygame.K_RIGHT]:
-        paddle.moveRight(5, 800) 
+        paddle.moveRight(5)
 
     # --- Game logic should go here
     all_sprites_list.update()
-    ball.advance()
 
-    #bounce off walls
-    if ball.center_x < 10 and ball.velocity_dx < 0:
-        ball.bounce_horizontal()
-    if ball.center_x > WIDTH - 10 and ball.velocity_dx > 0:
-        ball.bounce_horizontal()
-    if ball.center_y < 48 and ball.velocity_dy < 0:
-        ball.bounce_vertical()
-    if ball.center_y > HEIGHT and ball.velocity_dy > 0:
-        ball.bounce_vertical()
+    #Check if the ball is bouncing against any of the 4 walls:
+    if ball.rect.x>=790:
+        ball.velocity[0] = -ball.velocity[0]
+    if ball.rect.x<=0:
+        ball.velocity[0] = -ball.velocity[0]
+    if ball.rect.y>590:
+        ball.velocity[1] = -ball.velocity[1]
+        lives -= 1
+        if lives == 0:
+            #Display Game Over Message for 3 seconds
+            font = pygame.font.Font(None, 74)
+            text = font.render("GAME OVER", 1, WHITE)
+            screen.blit(text, (250,300))
+            pygame.display.flip()
+            pygame.time.wait(3000)
 
- 
+            #Stop the Game
+            carryOn=False
+
+    if ball.rect.y<40:
+        ball.velocity[1] = -ball.velocity[1]
+
+    #Detect collisions between the ball and the paddles
+    if pygame.sprite.collide_mask(ball, paddle):
+      ball.rect.x -= ball.velocity[0]
+      ball.rect.y -= ball.velocity[1]
+      ball.bounce()
+
+    #Check if there is the ball collides with any of bricks
+    brick_collision_list = pygame.sprite.spritecollide(ball,all_bricks,False)
+    for brick in brick_collision_list:
+      ball.bounce()
+      score += 1
+      brick.kill()
+      if len(all_bricks)==0:
+           #Display Level Complete Message for 3 seconds
+            font = pygame.font.Font(None, 74)
+            text = font.render("LEVEL COMPLETE", 1, WHITE)
+            screen.blit(text, (200,300))
+            pygame.display.flip()
+            pygame.time.wait(3000)
+
+            #Stop the Game
+            carryOn=False
+
     # --- Drawing code should go here
-    # First, clear the screen to dark blue. 
+    # First, clear the screen to dark blue.
     screen.fill(DARKBLUE)
     pygame.draw.line(screen, WHITE, [0, 38], [800, 38], 2)
-    ball.draw(screen, WHITE)
-
-    #Now let's draw all the sprites in one go. (For now we only have 2 sprites!)
-    all_sprites_list.draw(screen)
 
     #Display the score and the number of lives at the top of the screen
     font = pygame.font.Font(None, 34)
@@ -91,12 +139,15 @@ while carryOn:
     screen.blit(text, (20,10))
     text = font.render("Lives: " + str(lives), 1, WHITE)
     screen.blit(text, (650,10))
- 
+
+    #Now let's draw all the sprites in one go. (For now we only have 2 sprites!)
+    all_sprites_list.draw(screen)
+
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
-     
+
     # --- Limit to 60 frames per second
     clock.tick(60)
- 
+
 #Once we have exited the main program loop we can stop the game engine:
 pygame.quit()
